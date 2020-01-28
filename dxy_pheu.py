53a84ca84865d394b8a847aac4f87651f786bbac
Tooruchan <tooruchan@protonmail.com>
2020-01-28 10:10:08 +0800

    Update api

--- dxy_pheu.py
+++ dxy_pheu.py
@@ -8,10 +8,10 @@ from datetime import datetime,timedelta,timezone
 import pytz
 
 app = Client("pneumonia_bot",
-             proxy=dict(hostname="127.0.0.1", port=1090),
-             bot_token="939655223:AAH7iLIVCrQUAssICSYukHVkjsCOlsRPnP4",
-             api_id=135843,
-             api_hash='777bebb1fd9b98eb2980fd1e7c3de9ab')
+#             proxy=dict(hostname="127.0.0.1", port=1090),
+             bot_token="",
+             api_id=0,
+             api_hash='')
 
 headers = {
     'authority': '3g.dxy.cn',
@@ -20,7 +20,7 @@ headers = {
     'cookie': 'DXY_USER_GROUP=34',
 }
 def get_data():
-    global date,newstr,province_str,app,timestamp
+    global date,newstr,province_str,app,timestamp,stat_data
     response = requests.get('https://3g.dxy.cn/newh5/view/pneumonia',
                             headers=headers)
     response.encoding = 'utf-8'
@@ -30,20 +30,41 @@ def get_data():
                                     id="getStatisticsService")[0])[70:-20]
     stat_data = json.loads(stat_rawdata)
     pro_data = json.loads(province_rawdata)
-    numbers = re.findall(r"\d*", str(stat_data["countRemark"]))
     timestamp = stat_data["modifyTime"] / 1000
     # print(timestamp)
     loctime = datetime.fromtimestamp(timestamp).astimezone(timezone(timedelta(hours=8)))
     dt = loctime.strftime("%Y-%m-%d %H:%M")
     date = '#2019新型冠状病毒\n截至 **{0}** (UTC +8)数据统计'.format(dt)
-    while '' in numbers:
-        numbers.remove('')
-    # 数据格式：
-    # 全国 确诊 1372 例 疑似 1983 例 \n
-    # 死亡 41 例 治愈 39 例
-    # print(numbers)
+    
+    """
+    新数据格式：
+    {
+        "id": 1,
+        "createTime": 1579537899000,
+        "modifyTime": 1580175304000,
+        "infectSource": "野生动物，可能为中华菊头蝠",
+        "passWay": "经呼吸道飞沫传播，亦可通过接触传播",
+        "imgUrl": "https://img1.dxycdn.com/2020/0123/733/3392575782185696736-73.jpg",
+        "dailyPic": "https://img1.dxycdn.com/2020/0127/350/3393218957833514634-73.jpg",
+        "summary": "",
+        "deleted": false,
+        "countRemark": "",
+        "confirmedCount": 4474,
+        "suspectedCount": 5794,
+        "curedCount": 60,
+        "deadCount": 106,
+        "virus": "新型冠状病毒 2019-nCoV",
+        "remark1": "易感人群: 人群普遍易感。老年人及有基础疾病者感染后病情较重，儿童及婴幼儿也有发病",
+        "remark2": "潜伏期: 1~14 天均有，平均 10 天，潜伏期内存在传染性",
+        "remark3": "",
+        "remark4": "",
+        "remark5": "",
+        "generalRemark": "疑似病例数来自国家卫健委数据，目前为全国数据，未分省市自治区等",
+        "abroadRemark": ""
+    }
+    """
     newstr = "\n在中国境内确诊 **{0}** 例，疑似 **{1}** 例，死亡 **{2}** 例，治愈 **{3}** 例。\n".format(
-        numbers[0], numbers[1], numbers[2], numbers[3])
+        stat_data["confirmedCount"], stat_data["suspectedCount"], stat_data["deadCount"], stat_data["curedCount"])
     provinces = []
     for province in pro_data:
         #数据格式：
@@ -73,12 +94,13 @@ def get_data():
         provinces.append(seperated_province)
     province_str = '\n'.join(provinces)
 def sendmsg():
-    global date,newstr,province_str,app
+    global date,newstr,province_str,app,stat_data
     app.start()
-    app.send_message(
-        -1001146723984,
+    app.send_photo(
+        -1,
         #请在此处自行填写频道/群组ID，可以通过 Group Bulter 的 /id 命令来查询。
-        '\n'.join([date, newstr, province_str]),
+        photo=stat_data["dailyPic"],
+        caption='\n'.join([date, newstr, province_str]),
         parse_mode='md',
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("数据来源：丁香园", url="https://3g.dxy.cn/newh5/view/pneumonia")]]))
